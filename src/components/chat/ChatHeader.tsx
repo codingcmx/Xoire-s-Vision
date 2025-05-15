511960d07a311134e1e8b505b686f15b0c0bb8c1
Escape Matrix <matrixe866@gmail.com>
2025-05-15 10:10:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ChatHeader>
    
    with the text content:
    
        StyleBot
    
    Relevant files:
    
        - https://6000-firebase-studio-1747291506937.cluster-sumfw3zmzzhzkx4mpvz3ogth4y.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1747291506937.cluster-sumfw3zmzzhzkx4mpvz3ogth4y.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    rename StyleBot as StyleBot-Xoire

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -4,7 +4,7 @@ export function ChatHeader() {
   return (
     <header className="bg-primary text-primary-foreground p-4 shadow-md flex items-center space-x-2">
       <Bot className="h-7 w-7" />
-      <h1 className="text-xl font-semibold">StyleBot</h1>
+      <h1 className="text-xl font-semibold">StyleBot-Xoire</h1>
     </header>
   );
 }
